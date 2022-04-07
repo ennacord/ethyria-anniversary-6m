dd6285a42476116701880c5052caacee9f025244
Jet Rico <557312+dragonjet@users.noreply.github.com>
2022-04-07 13:20:22 -0700

    Add MB's intro video (#5)

--- src/main.js
+++ src/main.js
@@ -7,14 +7,16 @@ import store from './plugins/store';
 import vuetify from './plugins/vuetify';
 import './plugins/mobile';
 
+
+
 Vue.use(VueMasonryPlugin);
 
 Vue.config.devtools = false;
 Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  render: (h) => h(App),
+    router,
+    store,
+    vuetify,
+    render: (h) => h(App),
 }).$mount('#app');
