ff7568af4418fb2ef7f3376b4dca53e4f4725615
dragonjet <jetriconew@gmail.com>
2022-04-07 22:33:01 -0700

    Updated credits

--- src/components/SectionCredits.vue
+++ src/components/SectionCredits.vue
@@ -7,11 +7,11 @@
         </p>
         <p>
           Management: Ennacord staff<br />
-          Programming, Head message: jetrico<br />
+          Programming, Site header message: jetrico<br />
           Header &amp; Background Art: Hel<br />
-          Aloupeeps on Cards: Frank<br />
+          Aloupeeps on Cards: Frank Akanoru<br />
           Timeline committee: koshka-sova, Professor Nobu<br />
-          Popup Video: MB, Modal: AlphaBud<br />
+          Popup Video: MB, Vince, Modal: AlphaBud<br />
         </p>
         <p>
           <strong>Country Roads Acapella</strong><br />
@@ -19,21 +19,18 @@
           Arrangement: Soul Jam<br />
           Alternate Lyrics: Soul Jam, guGGy<br />
           Mix: Soul Jam<br />
-          Vocals: Acerola Heart, Binns, Crono, DarkDisasterKid, Dippy (Professional Simp),<br />
+          Vocals: Acerola Heart, Binns, Crono, DarkDisasterKid, Simpy Dippy,<br />
             Domo Espresso, Drumfeelies, hiya! kk, IcyMeerkat, Jo-さん, koshka-sova, Norenche,<br />
             Professor Nobu, Ralphy, Sasu Crow, Senno, Soul Jam, WhiteGuyGraal, zuiKatsu<br />
           Artwork: MinniJay, Professor Nobu, Soul Jam<br />
-          A production of the Enna Alouette Fan Server<br />
-          Project Manager: Domo Espresso<br />
-          Server Owner: MiikeMQ
         </p>
         <p>
           <strong>Art Quilt</strong><br />
           Base Design: Garfield Factory<br />
           Artists (in order, top-bottom, left-right):<br />
             reteach, Professor Nobu, Jo-さん, Erin, drum,<br />
-            wister, wax apple, Sorekasho, oja, Toast,<br />
-            mirrorline, Garfield Factory, Hel, Frank, Hexvati,<br />
+            lashe w, wax apple, Sorekasho, oja, Toast,<br />
+            mirrorline, Garfield Factory, Hel, Frank Akanoru, Hexvati,<br />
             jetrico, Voldox, MinniJay, no1, sojuice
         </p>
         <p>
@@ -42,6 +39,12 @@
           Programming: AlphaBud<br />
           Character Artworks: MinniJay, Hel<br />
           Background, Word Art: Professor Nobu<br />
+        </p>
+        <p>
+          A production of the Enna Alouette Fan Server<br />
+          Project Manager: Domo Espresso<br />
+          Server Owner: MiikeMQ<br />
+          and all Aloupeeps who celebrate these milestones with Enna and Ethyria!
         </p>
       </v-col>
     </v-row>
